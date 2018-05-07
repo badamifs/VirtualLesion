833c6ef4552b3ba9914a0f0e969f431e841367cd
Faizan Badami <badamifs@stanford.edu>
2018-05-06 19:59:31 -0700

    input-output arguments added

--- streamline_clustering.py
+++ streamline_clustering.py
@@ -8,24 +8,39 @@ import itertools
 import MRTrix2TrackVis
 import os
 
-f_in_nifti = os.path.join(path, subject, 'T1w/Diffusion/data.nii.gz')
-f_in_stream = os.path.join(directory_output, 'Lamyg2LpMFG_combined.tck')
-f_out_converted = os.path.join(directory_output, 'Lamyg2LpMFG_combined.trk')
-f_out_clustered = os.path.join(directory_output, 'Lamyg2LpMFG_clustered.trk')
-f_out_centroids = os.path.join(directory_output, 'Lamyg2LpMFG_centroids.trk')
+def clustering():
+	parser = argparse.ArgumentParser(prog='streamline_clustering.py',
+                                     description=__doc__)
+	parser.add_argument('-i', '--input', type=str, required=True,
+                        help="Raw data directory")
+	parser.add_argument('-o', '--output', type=str, required=True,
+                        help="Processed data directory")
+	args = parser.parse_args()
 
-MRTrix2TrackVis.convert_tck2trk(f_in_stream, f_in_nifti, f_out_converted)
+	input_dir = op.abspath(args.input)
+	directory_output = op.abspath(args.output)
 
-feature = ResampleFeature(nb_points=50)
-metric = AveragePointwiseEuclideanMetric(feature=feature)
-qb = QuickBundles(threshold=10., metric=metric)
-clusters = qb.cluster(streamlines)
+	f_in_nifti = os.path.join(input_dir, 'T1w/Diffusion/data.nii.gz')
+	f_in_stream = os.path.join(directory_output, 'Lamyg2LpMFG_combined.tck')
+	f_out_converted = os.path.join(directory_output, 'Lamyg2LpMFG_combined.trk')
+	f_out_clustered = os.path.join(directory_output, 'Lamyg2LpMFG_clustered.trk')
+	f_out_centroids = os.path.join(directory_output, 'Lamyg2LpMFG_centroids.trk')
 
-major_cluster = clusters > 60
-major_path = []
-for j in range(len(clusters)):
-    if major_cluster[j] == True:
+	#MRTrix2TrackVis.convert_tck2trk(f_in_stream, f_in_nifti, f_out_converted)
+	streams, hdr = tv.read(f_out_converted)
+    streamlines = [i[0] for i in streams]
+
+	feature = ResampleFeature(nb_points=50)
+	metric = AveragePointwiseEuclideanMetric(feature=feature)
+	qb = QuickBundles(threshold=10., metric=metric)
+	clusters = qb.cluster(streamlines)
+
+	major_cluster = clusters > 60
+	major_path = []
+	for j in range(len(clusters)):
+    	if major_cluster[j] == True:
         major_path.append([streamlines[i] for i in clusters[j].indices])
-major_streams = list(itertools.chain(*major_path))
-strm = ((sl, None, None) for sl in major_streams)
-tv.write(f_out_clustered, strm,  hdr_mapping=hdr)
+		major_streams = list(itertools.chain(*major_path))
+		strm = ((sl, None, None) for sl in major_streams)
+		tv.write(f_out_clustered, strm,  hdr_mapping=hdr)
+		print 'All done'
