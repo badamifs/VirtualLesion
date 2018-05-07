3ae7bbb161c3aa34f70b092d7ef1b8b26cf7706d
Faizan Badami <badamifs@stanford.edu>
2018-05-06 21:23:12 -0700

    spaces to tabs

--- streamline_clustering.py
+++ streamline_clustering.py
@@ -2,10 +2,13 @@ import numpy as np
 from nibabel import trackvis as tv
 from dipy.segment.clustering import QuickBundles
 import utilities
+from glob import glob
+from os import path as op
 from dipy.segment.metric import ResampleFeature
 from dipy.segment.metric import AveragePointwiseEuclideanMetric
 import itertools
 import MRTrix2TrackVis
+import argparse
 import os
 
 def clustering():
@@ -15,10 +18,13 @@ def clustering():
                         help="Raw data directory")
 	parser.add_argument('-o', '--output', type=str, required=True,
                         help="Processed data directory")
+	parser.add_argument('-s', '--subject_id', type=str, required=True,
+                        help="hcp subject id")
 	args = parser.parse_args()
 
 	input_dir = op.abspath(args.input)
 	directory_output = op.abspath(args.output)
+	subject_id = op.abspath(args.subject_id)
 
 	f_in_nifti = os.path.join(input_dir, 'T1w/Diffusion/data.nii.gz')
 	f_in_stream = os.path.join(directory_output, 'Lamyg2LpMFG_combined.tck')
@@ -28,7 +34,7 @@ def clustering():
 
 	#MRTrix2TrackVis.convert_tck2trk(f_in_stream, f_in_nifti, f_out_converted)
 	streams, hdr = tv.read(f_out_converted)
-    streamlines = [i[0] for i in streams]
+	streamlines = [i[0] for i in streams]
 
 	feature = ResampleFeature(nb_points=50)
 	metric = AveragePointwiseEuclideanMetric(feature=feature)
@@ -38,11 +44,12 @@ def clustering():
 	major_cluster = clusters > 60
 	major_path = []
 	for j in range(len(clusters)):
-    	if major_cluster[j] == True:
-        major_path.append([streamlines[i] for i in clusters[j].indices])
-		major_streams = list(itertools.chain(*major_path))
-		strm = ((sl, None, None) for sl in major_streams)
-		tv.write(f_out_clustered, strm,  hdr_mapping=hdr)
-		print 'All done'
+	    	if major_cluster[j] == True:
+		        major_path.append([streamlines[i] for i in clusters[j].indices])
+	major_streams = list(itertools.chain(*major_path))
+	strm = ((sl, None, None) for sl in major_streams)
+	tv.write(f_out_clustered, strm,  hdr_mapping=hdr)
+	print("Subject {n} done".format(n=subject_id))
+
 if __name__ == "__main__":
     clustering()
